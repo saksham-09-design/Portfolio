371da4bdee729e9b6020c4b4a9fad1dfc2d01774
Saksham <sakshamloona9@gmail.com>
2025-06-01 22:11:16 +0530

    Completed

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const im1 = document.getElementById("project-image1")
 const imcert = document.getElementById("card-cert")
 const hoversign = document.querySelector('.hover-sign')
 
+const sideBar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon')
+
 
 im1.addEventListener('mouseover',function(){
     hoversign.classList.add("active")
@@ -15,4 +19,15 @@ imcert.addEventListener('mouseover',function(){
 })
 imcert.addEventListener('mouseout',function(){
     hoversign.classList.remove("active")
+})
+
+menu.addEventListener("click", function(){
+    sideBar.classList.remove("close-sidebar")
+    sideBar.classList.add("open-sidebar")
+});
+
+closeIcon.addEventListener("click", function(){
+    sideBar.classList.remove("open-sidebar");
+    sideBar.classList.add("close-sidebar");
+    
 })
